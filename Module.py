56b03852a71c9e399b83af197884367252443513
Tawana2000 <murtezatawana8@gmail.com>
2024-08-23 14:06:32 -0700

    Add files via upload

---
+++ Module.py
@@ -0,0 +1,15 @@
+# Module is a file containing a set of functions and statements
+
+import math as m
+
+result = m.sqrt(3)
+print(result)
+
+pi = m.pi
+print(pi)
+
+
+import calculator as cal
+
+result1 = cal.add(2,6)
+print(result1)
