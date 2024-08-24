1c3cc2d5c3f65d7fa3a47a265fa00b92b740cd0a
Tawana2000 <murtezatawana8@gmail.com>
2024-08-24 04:11:31 -0700

    Add files via upload

---
+++ Arithematic-using-lambda.py
@@ -0,0 +1,6 @@
+#Sum of the square root of two numbers using a lambda function.
+
+compute = lambda n1,n2 : print((n1 ** (1/2)) + (n2 ** (1/2)))
+n1 = int(input())
+n2 = int(input())
+compute(n1,n2)
