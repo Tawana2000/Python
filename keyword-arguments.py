417ef611171bfb1f0c7e3cdbb1e7838e3137aa53
Tawana2000 <murtezatawana8@gmail.com>
2024-08-24 04:12:06 -0700

    Add files via upload

---
+++ keyword-arguments.py
@@ -0,0 +1,8 @@
+#Write a program to create a function that takes two arguments and call the function with keyword arguments
+def keyword(arg1, arg2):
+    print(arg1)
+    print(arg2)
+
+n1 = int(input())
+n2 = int(input())
+keyword(arg1 = n1, arg2 = n2)
