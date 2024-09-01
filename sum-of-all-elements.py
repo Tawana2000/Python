4342dcc22abb708111e8ae9f960a26492624cdc9
Tawana2000 <murtezatawana8@gmail.com>
2024-09-01 15:19:29 -0700

    Add files via upload

---
+++ sum-of-all-elements.py
@@ -0,0 +1,9 @@
+#Find the sum of all the elements of a list in Python. For example, if we have a list of numbers
+
+def sum_elements(list1):
+
+    result = sum(list1)
+    print(result)
+
+list1 = [5,6,7,8,23,51]
+sum_elements(list1)
