be9a3b6e1245807393a96c4284ef91f1e79421ea
Tawana2000 <murtezatawana8@gmail.com>
2024-09-02 02:07:57 -0700

    Add files via upload

---
+++ inserting-element-at-the-end-list.py
@@ -0,0 +1,9 @@
+#Write a Python program to insert an element at the end of the list of size n.
+
+def insert_element(list1):
+
+    list1.append(9)
+    print(list1)
+
+list1 = [5,2,13,72,4]
+insert_element(list1)
