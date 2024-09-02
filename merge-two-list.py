1bbc62028e4adc48440f42fa93257f0165b1d8cd
Tawana2000 <murtezatawana8@gmail.com>
2024-09-02 02:08:14 -0700

    Add files via upload

---
+++ merge-two-list.py
@@ -0,0 +1,10 @@
+#Create a Python program to merge two lists containing string elements.
+
+def merge(l1,l2):
+    
+    result = l1 + l2
+    return result
+
+l1 = ['apple','banana','mango']
+l2 = ['potato','brinjail','cauliflower']
+print(merge(l1,l2))
