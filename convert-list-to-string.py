2273a8a44fe654255b84b7caa17eaee10dbaab42
Tawana2000 <murtezatawana8@gmail.com>
2024-09-04 13:05:58 -0700

    Add files via upload

---
+++ convert-list-to-string.py
@@ -0,0 +1,7 @@
+#Given a list of size n, convert it to a string.
+#In python we can use the build in function join()
+
+list1 = ['Hello', 'world', 'from', 'Python']
+
+word = ' '.join(list1) #'' is used to specify the spaces between each element of the list 
+print(word)
