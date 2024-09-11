df00e8561645ddbcd98ca0a94a380009dd1d6656
Tawana2000 <murtezatawana8@gmail.com>
2024-09-11 14:05:52 -0700

    Add files via upload

---
+++ add-commas-to-separate-thousands.py
@@ -0,0 +1,19 @@
+# Python program to add commas to a number to separate the thousands
+
+def thousands_sep(number):
+
+    number = str(number) [::-1]    #convert the number to string and reverse
+
+    separated = ''   #empty string to store the result
+
+    for i in range(0, len(number)):
+
+        if i % 3 == 0 and i != 0:
+            separated += ','  #add comma to separated string
+
+        separated += number[i] #add number to the separated string
+
+    return separated[::-1] # reverse again to convert back to its original
+
+result = thousands_sep(10000000)
+print(result)
