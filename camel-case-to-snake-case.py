36c8bb1fffd80ac9915089ddbcbc4e132f12aa6b
Tawana2000 <murtezatawana8@gmail.com>
2024-09-16 18:36:19 -0700

    Add files via upload

---
+++ camel-case-to-snake-case.py
@@ -0,0 +1,25 @@
+#Now, write a Python program that converts camelCase strings to snake_case.
+
+def cam_snake(string):
+
+    snake_case = ''
+
+    for index, char in enumerate(string):
+
+        if char.isupper():
+
+            if index != 0:
+                snake_case += '_'
+
+            snake_case += char.lower()
+
+        else:
+
+            snake_case += char
+
+        
+    return snake_case
+
+
+string = input()
+print(cam_snake(string))
