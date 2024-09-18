542695afacfdd16a5984aec0075c607f9220db69
Tawana2000 <murtezatawana8@gmail.com>
2024-09-18 13:57:57 -0700

    Add files via upload

---
+++ correct-John's-english.py
@@ -0,0 +1,17 @@
+#John's primary language is Arabic, and his English is not so good. He has yet to learn about punctuations and letter casings. Now, write a Python program that cleans the string in such a way that it becomes readable. To do this,
+# Remove extra spaces from both ends. Capitalize only the first character. Add a dot at the end.
+
+def improve_english(string):
+
+    string = string.strip()  #removes spaces
+
+    string = string.capitalize()
+
+    string = string + '.'
+
+    return string
+
+
+string = input()
+print(improve_english(string))
+
