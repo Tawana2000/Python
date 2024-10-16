a6bba820940cd68fb84235342e218aece7803b0d
Tawana2000 <murtezatawana8@gmail.com>
2024-10-16 13:05:39 -0700

    Add files via upload

---
+++ sum-of-two-smallest-numbers.py
@@ -0,0 +1,13 @@
+#write a python function to find the sum of the two smallest number in a list
+
+def sum_of_smallest(numbers):
+
+    numbers.sort()
+
+    result = numbers[0] + numbers[1]
+
+    return result
+
+numbers = input().split()
+numbers = [int(n) for n in numbers]
+print(sum_of_smallest(numbers))
