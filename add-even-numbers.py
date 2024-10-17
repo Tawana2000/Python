a962270ed40cc0fee433778bfd476eced48943ff
Tawana2000 <murtezatawana8@gmail.com>
2024-10-17 14:28:09 -0700

    Add files via upload

---
+++ add-even-numbers.py
@@ -0,0 +1,13 @@
+#Write a function to add even numbers from 1 to n
+
+def add_even(n):
+
+    even_no = 0
+    for i in range(0, n+1):
+        if i % 2 == 0:
+            even_no += i
+
+    return even_no
+
+n = 10
+print(add_even(n))
