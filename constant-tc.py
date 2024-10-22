0b55daf50b3aac9dd748f40da92b336a8220354d
Tawana2000 <murtezatawana8@gmail.com>
2024-10-22 14:25:33 -0700

    Add files via upload

---
+++ constant-tc.py
@@ -0,0 +1,6 @@
+# If the execution time doesn't change by changing the input, is called constant time complexity (TC)
+
+i = 5
+print(i)
+
+#so whatever value we assign to i, the number of statements to be executed is 2, so TC = O(2), it's general practice to write O(1) for all constant time complexity, so O(1) = O(100) = O(1070)...
