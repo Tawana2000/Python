83ad38c8e9a0696e6e1c22b3c4593027580a534d
Tawana2000 <murtezatawana8@gmail.com>
2024-10-27 23:46:23 -0700

    Add files via upload

---
+++ cerciut-resistance.py
@@ -0,0 +1,10 @@
+# Write a function to calculate the total resistance in a series ciriut
+# of a series circuit, the total resistance is simply the sum of all individual resistance
+
+def resistance(values):
+
+    return sum(values)
+
+values = [5,10,15]
+
+print(resistance(values))
