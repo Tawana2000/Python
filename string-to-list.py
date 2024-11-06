c5b5ca63d9fb10e9c24678a907a0f38f4d7600d2
Tawana2000 <murtezatawana8@gmail.com>
2024-11-06 12:01:54 -0800

    Add files via upload

---
+++ string-to-list.py
@@ -0,0 +1,8 @@
+# Write a function that takes a string input from the user and converts it to a list of words
+
+def str_to_word(string):
+
+    return string.split()
+
+string = input("Enter a string: ")
+print(str_to_word(string))
