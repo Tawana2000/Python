2d1f280fa58d3c38a320ee4f7e7589dceb3cb74f
Tawana2000 <murtezatawana8@gmail.com>
2024-11-25 23:37:01 -0800

    Add files via upload

---
+++ show-data-type.py
@@ -0,0 +1,12 @@
+# Write a function to return the data type of a given argument
+
+def check_datatype(input):
+
+    return type(input)
+
+input = 'Hello'
+
+print(check_datatype(input))
+
+input = 23
+print(check_datatype(input))
