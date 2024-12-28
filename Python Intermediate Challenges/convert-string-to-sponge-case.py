d8ae23607e2fafa4bfc6791faca53f3328a984f4
Tawana2000 <murtezatawana8@gmail.com>
2024-12-28 13:57:01 -0800

    Add files via upload

---
+++ Python Intermediate Challenges/convert-string-to-sponge-case.py
@@ -0,0 +1,20 @@
+#Write a function to convert a given string to Spongecase
+
+def sponge_case(text):
+    result = ''
+    i = 0
+
+    for char in text:
+        if char.isalpha():
+            if i % 2 == 0:
+                result += char.lower()
+
+            else:
+                result += char.upper()
+
+            i += 1
+        else:
+            result += char
+    return result
+
+print(sponge_case('peace of mind'))
