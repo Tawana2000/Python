da0e8327c72c73b09f46ac87586ae97e562d8405
Tawana2000 <murtezatawana8@gmail.com>
2025-01-16 13:55:21 -0800

    Create longest-common-prefix.py

---
+++ Python Intermediate Challenges/longest-common-prefix.py
@@ -0,0 +1,14 @@
+# Write a function to find the longest common prefix among an array of strings.
+
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
+    prefix = strs[0]
+    for s in strs[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+    return prefix
+
+print(longest_common_prefix(["flower", "flow", "flight"]))
