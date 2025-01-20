3545397e3dd9b190bba8b92db834d63aabf4c9f8
Tawana2000 <murtezatawana8@gmail.com>
2025-01-19 22:23:34 -0800

    Create fibonacci-sequence.py

---
+++ Python Intermediate Challenges/fibonacci-sequence.py
@@ -0,0 +1,16 @@
+# Write a function to compute the nth Fibonacci number.
+def fibonacci(n):
+    if n <= 0:
+        return "Input should be a positive integer"
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, n):
+            a, b = b, a + b
+        return b
+
+n = 10 
+fibonacci(n)
