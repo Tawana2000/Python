8ea6eb4ec5de7a077b8b9d84930a610c39cfe1c8
Tawana2000 <murtezatawana8@gmail.com>
2025-01-21 13:43:41 -0800

    Create power-of-integer.py

---
+++ Python Intermediate Challenges/power-of-integer.py
@@ -0,0 +1,8 @@
+#Write a function to calculate the power of an integer
+
+def calculate_power(base, exponent):
+    
+    return base ** exponent
+base = int(input("Enter the base: "))
+exponent = int(input("Enter the exponent: "))
+print(calcualte_power(base, exponent))
