1d7918eaba138f816be2c13f3bd7c0c14163248a
Tawana2000 <murtezatawana8@gmail.com>
2025-01-24 12:26:25 -0800

    Add files via upload

---
+++ Python Intermediate Challenges/trace-of-a-matrix.py
@@ -0,0 +1,10 @@
+# Write a function to calculate the trace of a given matrix
+
+ #def calculate_trace(matrix):
+
+def sublists(n):
+
+    return [[n[0]] for _ in range(5)]
+
+n = [[3],[4]]
+print(sublists(n))
