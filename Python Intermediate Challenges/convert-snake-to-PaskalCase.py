43dfb15cbc67c0219096e620cef7a8eb9eeb5a6b
Tawana2000 <murtezatawana8@gmail.com>
2025-01-29 13:10:04 -0800

    Add files via upload

---
+++ Python Intermediate Challenges/convert-snake-to-PaskalCase.py
@@ -0,0 +1,30 @@
+#Create a Python program that converts snake_case strings to PascalCase.
+#In snake case, all words are in lowercase but separated by an underscore. For example, this_is_written_in_snake_case.
+#In pascal case, each word is separated by the first uppercase letter. For example, ThisIsWrittenInPascalCase.
+
+def pas_case(string):
+
+    new_str = ''
+    counter = 0   #to count the string characters
+
+    while counter < len(string):
+
+        if counter == 0:
+
+                new_str += string[counter].upper()
+
+        elif string[counter] == '_':
+             new_str += string[counter + 1].upper()
+
+             counter += 1
+        
+        else:
+             
+             new_str += string[counter]
+
+        counter += 1
+
+    return new_str
+
+result = pas_case('custom_date_builder')
+print(result)
