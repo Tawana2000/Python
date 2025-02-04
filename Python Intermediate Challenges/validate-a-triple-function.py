165ffe5e931c9086a399fac348f0547a75382ecb
Tawana2000 <murtezatawana8@gmail.com>
2025-02-04 13:06:07 -0800

    Create validate-a-triple-function.py

---
+++ Python Intermediate Challenges/validate-a-triple-function.py
@@ -0,0 +1,8 @@
+# Write a function to validate a triple function
+
+def triple_funciton(a, b, c):
+
+    return True if a and b and c else False
+
+print(triple_funciton(True, True, True))
+print(triple_funciton(True, False, False))
