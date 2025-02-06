87a7c23c984d8ecd2490fc255d00af4b5e8a8c42
Tawana2000 <murtezatawana8@gmail.com>
2025-02-06 13:43:10 -0800

    Add files via upload

---
+++ Python Intermediate Challenges/binary-string-to-decimal.py
@@ -0,0 +1,7 @@
+# Write a function to take a binary number string as input and convert it to its decimal equivalent
+
+def virtual_dac(binary):
+
+    return int(binary, 2)
+
+print(virtual_dac('1010'))
