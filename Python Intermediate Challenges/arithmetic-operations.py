76f6f85001a5f614eff72dc7a310d9c88a74f0cd
Tawana2000 <murtezatawana8@gmail.com>
2025-02-08 14:06:23 -0800

    Add files via upload

---
+++ Python Intermediate Challenges/arithmetic-operations.py
@@ -0,0 +1,12 @@
+# Write a function to perform arithmetic operations on two numbers
+
+def arithmetic_operations(a, b):
+
+    return {
+        'addition': a + b,
+        'Subtration': a - b,
+        'Multiplication': a * b,
+        'Division': a / b
+    }
+
+print(arithmetic_operations(5, 2))
