8610450e2a41a5ae011618212a07c37c14858899
Tawana2000 <murtezatawana8@gmail.com>
2025-02-11 13:32:33 -0800

    Add files via upload

---
+++ Python Intermediate Challenges/standard-form-sentence.py
@@ -0,0 +1,8 @@
+# Write a function to convert a given sentence into its standard form
+
+def standard_form(sentence):
+
+    return sentence.title()
+
+sentence = 'tHis IS A tesT sEntence'
+print(standard_form(sentence))
