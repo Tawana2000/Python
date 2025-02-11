314a0ad8ea64f49256c3747e6fff5c0de810cc61
Tawana2000 <murtezatawana8@gmail.com>
2025-02-10 23:36:54 -0800

    Add files via upload

---
+++ Python Intermediate Challenges/equal-string.py
@@ -0,0 +1,7 @@
+# Write a function to check if two strings are equal or not
+
+def are_strings_equal(str1, str2):
+
+    return True if str1 == str2 else False
+
+print(are_strings_equal('key', 'keys'))
