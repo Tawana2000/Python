b6819a976effe69786427bad65f981b4da3e3ba3
Tawana2000 <murtezatawana8@gmail.com>
2025-02-14 13:21:01 -0800

    Add files via upload

---
+++ Python Intermediate Challenges/scottish-screaming.py
@@ -0,0 +1,8 @@
+# Write a function to convert a given setence into Scottish Screaming which is basically the sentence written in uppercase
+
+def scottish_screaming(sentence):
+
+    return sentence.upper()
+
+sentence = 'Hello World'
+print(scottish_screaming(sentence))
