1374bd10f94c2da0be9585cccc5b58db6d03746d
Tawana2000 <murtezatawana8@gmail.com>
2025-02-16 14:12:27 -0800

    Add files via upload

---
+++ Python Intermediate Challenges/all-prime.py
@@ -0,0 +1,14 @@
+# Write a function to check if all numbers in a given list are prime
+
+def all_primes(numbers):
+
+    for num in numbers:
+
+        if num < 2 or any(num % i == 0 for i in range(2, int(num ** 0.5) + 1)):
+            return False
+        
+    return True
+
+
+print(all_primes([2, 3, 5]))
+print(all_primes([4, 8, 7, 13]))
