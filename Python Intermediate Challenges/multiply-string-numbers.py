08f456e8924f956901a01fa19f1c9c59e7a8ca1f
Tawana2000 <murtezatawana8@gmail.com>
2025-02-19 13:36:59 -0800

    Add files via upload

---
+++ Python Intermediate Challenges/multiply-string-numbers.py
@@ -0,0 +1,7 @@
+# Write a function to multiply two numbers respresented as strings
+
+def multiply_strings(num1, num2):
+
+    return str(int(num1) * int(num2))
+
+print(multiply_strings('123', '456'))
