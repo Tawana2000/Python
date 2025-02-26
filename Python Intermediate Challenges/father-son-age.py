990a32b6310c6f2c3654fdcec0adea38fa2e78fd
Tawana2000 <murtezatawana8@gmail.com>
2025-02-25 20:09:30 -0800

    Add files via upload

---
+++ Python Intermediate Challenges/father-son-age.py
@@ -0,0 +1,7 @@
+# Write a function to calculate when the father's age will be twice the son's age
+
+def double_age(father_age, son_age):
+
+    return (father_age - son_age) - son_age
+
+print(double_age(36, 7))
