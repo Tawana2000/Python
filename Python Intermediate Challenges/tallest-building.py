8cd6caf5ed113de648b7be1a25f5034f54801833
Tawana2000 <murtezatawana8@gmail.com>
2025-03-06 13:57:17 -0800

    Add files via upload

---
+++ Python Intermediate Challenges/tallest-building.py
@@ -0,0 +1,7 @@
+# Write a function to find the height of the tallest building from a list of heights
+
+def find_talles(heights):
+
+    return max(heights)
+
+print(find_talles([100, 200, 150, 300]))
