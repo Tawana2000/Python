99bde28a88fcd7ec872b26cbc4b9246e2f7ea32f
Tawana2000 <murtezatawana8@gmail.com>
2025-03-18 14:34:36 -0700

    Add files via upload

---
+++ Python Advance Challenges/two-sum.py
@@ -0,0 +1,14 @@
+# Write a function to return indices of the two numbers in an array such that they add up to a given target
+
+def two_sum(nums, target):
+
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+            
+
+    return []
+
+print(two_sum([6, 3, 21, 18], 24))
+print(two_sum([8, 31, 16, 83, 25], 60))
