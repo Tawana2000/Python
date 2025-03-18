7a99bf1e021c50d9b5148b0711f2d6e9d4f23368
Tawana2000 <murtezatawana8@gmail.com>
2025-03-18 14:33:51 -0700

    Add files via upload

---
+++ Python Advance Challenges/is-harshad-number.py
@@ -0,0 +1,18 @@
+# Write a function to check if a number is a Harshad number or not
+# A Harshad number in a given number base is an integer that is divisible by the sum of its digits when written in that base
+
+def is_harshad(n):
+    
+    result = 0
+    for num in str(n):
+
+        result += int((num))
+
+    if n % result == 0:
+        return True
+    
+    else:
+        return False
+    
+print(is_harshad(18))
+print(is_harshad(28))
