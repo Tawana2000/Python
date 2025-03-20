ff1909f4dd4298676e2c35a4985617ff9f2400cc
Tawana2000 <murtezatawana8@gmail.com>
2025-03-20 14:12:43 -0700

    Add files via upload

---
+++ Python Advance Challenges/flipping-bits.py
@@ -0,0 +1,13 @@
+# Write a function to flip the bits of a given integer
+
+def flip_bits(n):
+
+    num_bits = n.bit_length()
+    
+    mask = (1 << num_bits) - 1
+    
+    return n ^ mask
+
+
+print(flip_bits(5))
+print(flip_bits(0))
