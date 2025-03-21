8556beed8849488f5bb0f7feff15aca7889a4a90
Tawana2000 <murtezatawana8@gmail.com>
2025-03-21 14:21:21 -0700

    Add files via upload

---
+++ Python Advance Challenges/factorians.py
@@ -0,0 +1,16 @@
+# Write a functio to find all the factorians within a range
+# A factorian is a natural number that equals the sum of the facotrials of its decimal digits. For example, 145 is a factorian because 1! + 4! + 5! = 145
+
+import math 
+def find_factorians(start, end):
+
+    result = []
+
+    for i in range(start, end + 1):
+        if sum(math.factorial(int(d)) for d in str(i)) == i:
+            result.append(i)
+
+
+    return result
+
+print(find_factorians(1, 500))
