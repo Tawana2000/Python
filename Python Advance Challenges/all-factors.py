33a34e4874194213162e1f0fe9b50e6fca81311d
Tawana2000 <murtezatawana8@gmail.com>
2025-03-22 14:10:00 -0700

    Add files via upload

---
+++ Python Advance Challenges/all-factors.py
@@ -0,0 +1,13 @@
+# Write a function to find all factors of a given number
+
+def get_factors(n):
+
+    facotrs = []
+
+    for i in range(1, n + 1):
+        if n % i == 0:
+            facotrs.append(i)
+
+    return facotrs
+
+print(get_factors(10))
