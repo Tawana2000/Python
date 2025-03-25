44ee6332066f37d2c52416e6add53cfa38eeb279
Tawana2000 <murtezatawana8@gmail.com>
2025-03-25 13:43:00 -0700

    Add files via upload

---
+++ Python Advance Challenges/count-all-distinct-pairs.py
@@ -0,0 +1,10 @@
+# Write a function to count all distinct pairs in an list with difference equal to k
+
+def count_pairs(numbers, k):
+
+    numbers_set = set(numbers)
+    count = sum(1 for numbers in numbers_set if numbers + k in numbers_set)
+    return count
+
+
+print(count_pairs([1, 5, 3, 4, 2], 2))
