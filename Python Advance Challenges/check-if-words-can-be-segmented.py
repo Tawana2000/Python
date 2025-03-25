0c462e3b40744f47cc79320c1478afb3ddaef884
Tawana2000 <murtezatawana8@gmail.com>
2025-03-25 13:43:34 -0700

    Add files via upload

---
+++ Python Advance Challenges/check-if-words-can-be-segmented.py
@@ -0,0 +1,8 @@
+# Write a function to check if four given words can be completely segmented from the string
+
+def can_segment_string(s, words = ['apple', 'pear', 'pier', 'pie']):
+
+    return s == "" or any(s.startswith(w) and can_segment_string(s[len(w):]) for w in words)
+
+print(can_segment_string("applepiearpier"))
+print(can_segment_string("applepiepier"))  
