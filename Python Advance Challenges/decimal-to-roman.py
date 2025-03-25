3e126d81d67780e92c05e3da7411ee1f034a7713
Tawana2000 <murtezatawana8@gmail.com>
2025-03-24 22:45:47 -0700

    Add files via upload

---
+++ Python Advance Challenges/decimal-to-roman.py
@@ -0,0 +1,16 @@
+# Write a function to convert a decimal number lying between 1 and 3999 to Roman numerals
+
+def decimal_to_roman(num):
+
+    roman = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 
+             50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+    
+    result = ""
+
+    for i in roman:
+        result += roman[i] * (num // i)
+        num %= i
+
+    return result
+
+print(decimal_to_roman(1880))
