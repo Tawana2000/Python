6b814780c1449943d5438d28ac7360569c6f9991
Tawana2000 <murtezatawana8@gmail.com>
2025-03-25 13:43:18 -0700

    Add files via upload

---
+++ Python Advance Challenges/maximum-number-of-points.py
@@ -0,0 +1,21 @@
+# Write a function to find the maximum number of points that lie on the same straight line
+
+def max_points_on_line(points):
+
+    if not points:
+        return 0
+    
+    max_points = 1
+
+    for i in range(len(points)):
+        slopes = {}
+        for j in range(i + 1, len(points)):
+            dx, dy = points[j][0] - points[i][0], points[j][1] - points[i][1]
+            slope = dy / dx if dx else "inf"
+            slopes[slope] = slopes.get(slope, 1) + 1
+
+        max_points = max(max_points, max(slopes.values(), default = 1))
+
+    return max_points
+
+print(max_points_on_line([(0,0), (1, 1), (2, 2)]))
