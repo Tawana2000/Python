aa8483e706354f781d5efd947e8f5f5617ca9a1b
Tawana2000 <murtezatawana8@gmail.com>
2025-03-25 13:42:44 -0700

    Add files via upload

---
+++ Python Advance Challenges/excel-sheet-column-number.py
@@ -0,0 +1,12 @@
+# Write a function to convert an Excel sheet column title to its corresponding column number
+
+def excel_column_number(column_title):
+
+    result = 0
+
+    for char in column_title:
+        result = result * 26 + (ord(char) - ord('A') + 1)
+
+    return result
+
+print(excel_column_number('AB'))
