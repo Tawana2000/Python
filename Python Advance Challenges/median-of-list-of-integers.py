d153eb74371126f0156f070b41576c4dbadfd61d
Tawana2000 <murtezatawana8@gmail.com>
2025-03-28 12:30:53 -0700

    Add files via upload

---
+++ Python Advance Challenges/median-of-list-of-integers.py
@@ -0,0 +1,11 @@
+# Write a function to find the median of a list of integers 
+
+def find_median(lst):
+
+    lst.sort()
+
+    median = len(lst) // 2
+    return (lst[median] + lst[median - 1]) / 2 if len(lst) % 2 == 0 else lst[median]
+
+print(find_median([1, 3, 2]))
+print(find_median([1, 6, 4, 2]))
