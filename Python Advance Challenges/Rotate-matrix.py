46b5094a2d486a2358f163eb7c2360a5bbbb5ca3
Tawana2000 <murtezatawana8@gmail.com>
2025-03-31 19:16:31 -0700

    Create Rotate-matrix.py

---
+++ Python Advance Challenges/Rotate-matrix.py
@@ -0,0 +1,15 @@
+# Write a function to rotate a square matrix 90 degrees clockwise in place.
+
+def rotate_matrix(matrix):
+    n = len(matrix)
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - j - 1][i]
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+            matrix[j][n - i - 1] = temp
+    return matrix
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(rotate_matrix(matrix)) 
