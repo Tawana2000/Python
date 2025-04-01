a116188d7a792c0c84886036d40f0642d5a9e83c
Tawana2000 <murtezatawana8@gmail.com>
2025-04-01 16:00:15 -0600

    Add files via upload

---
+++ Python Advance Challenges/accumulating-sum-of-numbers.py
@@ -0,0 +1,12 @@
+# Write a funtion to calculate the accumulating sum of list of numbers
+
+def running_sum(numbers):
+    result = []
+    current_sum = 0
+    for num in numbers:
+        current_sum += num
+        result.append(current_sum)
+    return result
+
+# Test
+print(running_sum([1, 2, 3, 4]))
