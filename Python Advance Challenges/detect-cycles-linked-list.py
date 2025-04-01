0abdb9b9a30afb9b74b3753c49ed0dd436dd2be5
Tawana2000 <murtezatawana8@gmail.com>
2025-03-31 19:21:28 -0700

    Create detect-cycles-linked-list.py

---
+++ Python Advance Challenges/detect-cycles-linked-list.py
@@ -0,0 +1,16 @@
+# Write a function to detect if a linked list has a cycle in it.
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def has_cycle(head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
+  
