7fe01c11cb1d67a749f86380205ec11ffefa7d13
Tawana2000 <murtezatawana8@gmail.com>
2025-04-04 15:06:59 -0600

    Add files via upload

---
+++ Python Advance Challenges/euler-zigzag.py
@@ -0,0 +1,23 @@
+# Write a function to calculate the nth Euler Zigzag number
+# Euler's zigzag numbers (or up/down numbers), denoted E(n), are a sequence of integers that describe the number of permutations of [1, ...., n] that are alternating (or "zig-zagging")
+
+def euler_zigzag(n):
+
+    if n < 0:
+        return 0
+    if n <= 1:
+        return 1
+    
+    prev2, prev1 = 1, 1
+    
+    for i in range(2, n + 1):
+        if i % 2 == 0:
+            prev2, prev1 = prev1, prev1 + prev2
+
+        else:
+            prev2, prev1 = prev1, prev1 * 2
+
+    return prev1
+
+
+print(euler_zigzag(4))
