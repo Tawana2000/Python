097407cdef98f281af122d20a7f48f7e9d1b420c
Tawana2000 <murtezatawana8@gmail.com>
2025-04-06 21:47:35 -0700

    Create word-break-problem.py

---
+++ Python Advance Challenges/word-break-problem.py
@@ -0,0 +1,9 @@
+# Given a string and a dictionary of words, determine if the string can be segmented into space-separated words from the dictionary.
+
+def word_break(s, wordDict):
+
+    pass
+
+print(word_break("leetcode", ["leet", "code"])) 
+print(word_break("applepen", ["apple", "pen"])) 
+print(word_break("catsandog", ["cats", "dog", "sand", "and", "cat"]))
