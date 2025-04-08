5c7fb6d1b09b0b2aaf6aacd7bac6b25eb3b6f5f3
Tawana2000 <murtezatawana8@gmail.com>
2025-04-08 13:28:24 -0700

    Add files via upload

---
+++ Python Advance Challenges/binary-toggle.py
@@ -0,0 +1,10 @@
+# Write a function to swap 0s and 1s from the binary representation of a number
+
+def binary_toggle(n):
+
+    if n < 0:
+        return n
+    
+    return n ^ ((1 << (n.bit_length() or 1)) - 1)
+
+print(binary_toggle(5))
