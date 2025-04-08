e0221b2576817e0c179f968294297c4949334b33
Tawana2000 <murtezatawana8@gmail.com>
2025-04-07 23:26:52 -0700

    Update rotate-matrix.py

--- Python Advance Challenges/rotate-matrix.py
+++ Python Advance Challenges/rotate-matrix.py
@@ -1,10 +1,16 @@
 # Write a function that rotates an NxN matrix 90 degrees clockwise in-place (without using extra space beyond O(1)).
 
 def rotate_matrix(matrix):
+    n = len(matrix)
 
-    pass
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        matrix[i].reverse()
 
 
 matrix = [[1,2,3], [4,5,6], [7,8,9]]
 rotate_matrix(matrix)
-print(matrix) 
+print(matrix)
