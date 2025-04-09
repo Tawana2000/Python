27fddbfc9aa7b7e57832ab88cefeb136814ad4b4
Tawana2000 <murtezatawana8@gmail.com>
2025-04-09 13:10:44 -0700

    Add files via upload

---
+++ repeat-last-character.py
@@ -0,0 +1,7 @@
+# Write a function to repeat the last character of a string n times
+
+def repeat_last_character(string, n):
+
+     return string[:-1] +  string[-1] * n
+
+print(repeat_last_character('hello', 3))
