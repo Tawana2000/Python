fc922037946607cac26d5072585b5348f1a26e07
Tawana2000 <murtezatawana8@gmail.com>
2025-04-11 13:20:20 -0700

    Add files via upload

---
+++ Python Basic Challenges/combine-two-lists.py
@@ -0,0 +1,9 @@
+# Write a function to combine two lists
+
+def fruit_salad(list1, list2):
+
+    return list1 + list2
+
+list1 = ['apple', 'banana']
+list2 = ['orange', 'grape']
+print(fruit_salad(list1, list2))
