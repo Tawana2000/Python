f78a6eef2197e065bb72714233be951cedd3b9a6
Tawana2000 <murtezatawana8@gmail.com>
2025-04-17 12:33:57 -0700

    Add files via upload

---
+++ Python Basic Challenges/full-name.py
@@ -0,0 +1,7 @@
+# Write a function to return a full name with space in between.
+
+def full_name(first_name, last_name):
+
+    return first_name + ' ' + last_name
+
+print(full_name('John', 'Doe'))
