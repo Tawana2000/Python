f22546d5f92b3d7cc75e0eae497987f3d047339a
Tawana2000 <murtezatawana8@gmail.com>
2025-04-19 12:39:09 -0700

    Add files via upload

---
+++ Python Basic Challenges/max-value-calculation.py
@@ -0,0 +1,19 @@
+# Write a function to calculate the maximum value using '+' or '*' sign between two numbers in a string
+
+def max_value(s):
+
+    first_digit = s[0]
+    second_digit = s[1]
+    sum = int(first_digit) + int(second_digit)
+    product = int(first_digit) * int(second_digit)
+    return sum if sum > product else product
+
+print(max_value('34'))
+
+
+# or a better way
+"""
+a, b = map(int, s.split())
+    return max(a + b, a * b)
+    
+"""
