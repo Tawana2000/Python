6e88ca9b7187ab981e829d298053631aea87232d
Tawana2000 <murtezatawana8@gmail.com>
2025-04-24 12:38:06 -0700

    Add files via upload

---
+++ Python Basic Challenges/number-of-people-in-train.py
@@ -0,0 +1,6 @@
+# Write a function to calculate the maximum number of people than can fit in a trian carriage.
+
+def carriage_capacity(seats, standing):
+    return seats + standing
+
+print(carriage_capacity(50, 20))
