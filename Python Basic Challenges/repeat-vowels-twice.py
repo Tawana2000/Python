a012a8dd2a0ed82c96945e566ac26adb18ed246e
Tawana2000 <murtezatawana8@gmail.com>
2025-04-29 13:04:34 -0700

    Add files via upload

---
+++ Python Basic Challenges/repeat-vowels-twice.py
@@ -0,0 +1,14 @@
+# Write a function to repeat all vowels twice in a given string
+
+def repeat_vowels(string):
+
+    vowels = 'AEIOUaeiou'
+    new_str = ''
+    
+    for char in string:
+        new_str += char
+        if char in vowels:
+            new_str += char
+    return new_str
+
+print(repeat_vowels('hello'))
