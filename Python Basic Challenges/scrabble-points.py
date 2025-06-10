5d71c1fd6cb90f7683e382639d50826d1adcd1a8
Tawana2000 <murtezatawana8@gmail.com>
2025-06-10 14:13:16 -0400

    Add files via upload

---
+++ Python Basic Challenges/scrabble-points.py
@@ -0,0 +1,10 @@
+# Write a function to calculate the points of a scrabble word.
+def calculate_points(word):
+    letter_points = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2,
+                     'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1,
+                     'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1,
+                     'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+    total_points = sum(letter_points.get(char, 0) for char in word.lower())
+    return total_points
+
+print(calculate_points("hello"))
