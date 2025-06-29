14e5ec37a134f3571c21fa2289e4dd8be2af7ba7
Tawana2000 <murtezatawana8@gmail.com>
2025-06-29 13:54:01 -0700

    Update covert-list-to-set.py

--- covert-list-to-set.py
+++ covert-list-to-set.py
@@ -1,7 +1,7 @@
 #Create a Python program to convert a list into a set.
 
 list1 = ['USA', 'Japan', 'Nepal', 'Japan']
-list2 = ['USA', 'Japan', 'Nepal', 'Japan']
+list2 = ['APPLE', 'PHONE', 'SALT', 'KEYBOARD']
 
 print(set(list1))
 print(set(list2))
