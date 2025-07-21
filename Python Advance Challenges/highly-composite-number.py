264720342dfd99c2b9a4b9c4d1c3d578cfb6efcb
Tawana2000 <murtezatawana8@gmail.com>
2025-07-21 12:30:43 -0700

    Update highly-composite-number.py

--- Python Advance Challenges/highly-composite-number.py
+++ Python Advance Challenges/highly-composite-number.py
@@ -1,8 +1,15 @@
-# Write a function to check if a number is a highly composite number.
-# A higly composite number is a positive integer with more divisors than any smaller positive integer has
+# Write a function to check if a number is highly composite number.
+# A highly composite number is a positive integer with more divisors than any smaller positive integer
 
 def is_highly_composite(n):
-  n_divisors = sum(1 for i in range(1, n+1) if n % i == 0)
-  for i in range(1, n):
-    if i_divisors >= n_disisors:
-      return False
+    n_divisors = sum(1 for i in range(1, n + 1) if n % i == 0)
+    
+    for i in range(1, n):
+        i_divisors = sum(1 for j in range(1, i + 1) if i % j == 0)
+        if i_divisors >= n_divisors:
+            return False
+    return True
+
+# Test case
+print(is_highly_composite(12))  # Output: True
+print(is_highly_composite(9))
