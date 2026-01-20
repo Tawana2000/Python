3cfab273b8627ecb2d3337c7909fd44a264cc9ab
Phantom <murtezatawana8@gmail.com>
2026-01-20 13:05:56 -0800

    Update fizzbuzz.py

--- Python Intermediate Challenges/fizzbuzz.py
+++ Python Intermediate Challenges/fizzbuzz.py
@@ -1,4 +1,4 @@
-
+#Print numbers 1-100, but for multiples of 3 print "Fizz", for multiples of 5 print "Buzz", and for multiples of both print "FizzBuzz".
 def fizzbuzz(n):
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
