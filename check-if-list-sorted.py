7527334e0f3bf75fbf292ef063d9c3a542bbbc9a
Phantom <murtezatawana8@gmail.com>
2026-01-26 00:24:56 -0600

    Refactor is_sorted function for clarity
    Removed unnecessary whitespace and simplified return statement.

--- check-if-list-sorted.py
+++ check-if-list-sorted.py
@@ -1,9 +1,6 @@
 # Write a function to check if a given list of numbers is sorted in ascending order
 
 def is_sorted(numbers):
-
     return True if sorted(numbers) == numbers else False
-
 numbers = list(map(int, input('Enter the list elements separated by space: ').split()))
-
 print(is_sorted(numbers))
