08fd3bc3997c645699a26e3d5d990ae7043ec8f2
Phantom <murtezatawana8@gmail.com>
2026-02-01 01:24:54 -0600

    Clean up blank lines in division program
    Removed unnecessary blank lines in the division program.

--- division-with-exception-handling.py
+++ division-with-exception-handling.py
@@ -1,8 +1,6 @@
 #program to divide numerator by denominator. However, if the denominator is 0, print Denominator cannot be 0. Try again.
 
 try:
-
-
     numerator = int(input())
     denominator = int(input())
 
