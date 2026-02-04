be1ae4879f4e836a717c84439b681ebda196dbd8
Phantom <murtezatawana8@gmail.com>
2026-02-03 21:55:00 -0800

    Clean up count-all-distinct-pairs.py
    Removed unnecessary blank lines at the end of the file.

--- Python Advance Challenges/count-all-distinct-pairs.py
+++ Python Advance Challenges/count-all-distinct-pairs.py
@@ -5,6 +5,4 @@ def count_pairs(numbers, k):
     numbers_set = set(numbers)
     count = sum(1 for numbers in numbers_set if numbers + k in numbers_set)
     return count
-
-
 print(count_pairs([1, 5, 3, 4, 2], 2))
